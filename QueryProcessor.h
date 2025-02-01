1e07e7cf8ec90daf902e7d13571df0778a2d1a48
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-01 09:08:22 +0530

    refactor: added leftJoin method

--- QueryProcessor.h
+++ QueryProcessor.h
@@ -2,7 +2,14 @@
 #define QUERY_PROCESSOR_H
 
 #include "Table.h"
+#include <iostream>
 
 void executeQuery(Table& table, const std::string& query);
+
+class QueryProcessor {
+public:
+    // Performs a LEFT JOIN on two tables based on a common column
+    static Table leftJoin(const Table& leftTable, const Table& rightTable, const std::string& joinColumn);
+};
 
 #endif
